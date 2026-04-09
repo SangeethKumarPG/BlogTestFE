16a9caec35bde2613fa3a2f882571ed048800dc5
Summya T.S <sumayyasalim2002@gmail.com>
2026-04-09 20:18:04 +0530

    delete endpoint updated

--- delete.js
+++ delete.js
@@ -1,12 +1,11 @@
-
-  await refreshPosts();
-  
-  document.addEventListener('click', async e => {
-    if (e.target.classList.contains('delete-btn')) {
-      const id = e.target.dataset.postId;
-      if (confirm('Are you sure?  this action cannot be undone')) {
-        await fetch(`https://test.spensol.com/posts/${id}`, {method: 'DELETE'});
-        await refreshPosts();
-      }
-    }
-  });
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('click', async e => {
+        if (e.target.classList.contains('delete-btn')) {
+            const id = e.target.dataset.postId;
+            if (confirm('Are you sure?')) {
+                await fetch(`https://test.spensol.com/posts/${id}`, {method: 'DELETE'});
+               
+            }
+        }
+    });
+});
